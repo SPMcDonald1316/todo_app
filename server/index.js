32af097c1d9b687f7387bbdb7d4a5655e42f044f
Sean McDonald <spmcdonald1316@gmail.com>
2020-08-22 16:19:25 -0500

    build server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(4000, () => {
+  console.log('Listening on port 4000');
+});
+
