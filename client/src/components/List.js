7945b8510fecef44527d5ca5f474a18a2047cf77
Sean McDonald <spmcdonald1316@gmail.com>
2020-08-29 11:50:23 -0500

    code functionality to grab todos from database

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,14 +1,31 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
 
 const List = () => {
+
+  const [todos, setTodos] = useState([])
+
+  const getTodos = async() => {
+    try {
+      const response = await fetch("http://localhost:4000/todos")
+      const responseJSON = await response.json();
+      setTodos(responseJSON);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
   return (
     <Fragment>
-      <table className="table">
+      <table className="table mt-5 text-center">
         <thead>
           <tr>
-            <th></th>
-            <th></th>
-            <th></th>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
           </tr>
         </thead>
         <tbody>
