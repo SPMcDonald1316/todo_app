8ee50f340002dc07f9bc582a0afdaddd7ae4cfab
Sean McDonald <spmcdonald1316@gmail.com>
2020-08-29 09:25:57 -0500

    add functionality to input to add todo to database

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,10 +1,28 @@
 import React, {Fragment, useState} from 'react'
 
 const Input = () => {
+
+  const [description, setDescription] = useState('');
+
+  const onSubmit = async event => {
+    event.preventDefault();
+    try {
+      const body = { description }
+      const response = await fetch("http://localhost:4000/todos", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(body)
+      });
+      console.log(response);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
   return (
     <Fragment>
       <h1 className="text-center mt-5">Todo List</h1>
-      <form className="d-flex mt-5">
+      <form className="d-flex mt-5" onSubmit={onSubmit}>
         <input 
           type="text" 
           className="form-control" 
