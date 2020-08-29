4316e0e13e350bdf60d63cd3e4ec84fde1093dc5
Sean McDonald <spmcdonald1316@gmail.com>
2020-08-29 11:03:10 -0500

    remove unused variable

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -8,7 +8,7 @@ const Input = () => {
     event.preventDefault();
     try {
       const body = { description }
-      const response = await fetch("http://localhost:4000/todos", {
+      await fetch("http://localhost:4000/todos", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(body)
